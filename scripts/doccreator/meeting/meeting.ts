1b273d0f864f9ccb78010c59d1024757712d1227
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-31 15:57:20 +0200

    Fix meeting file file format

--- scripts/doccreator/meeting/meeting.ts
+++ scripts/doccreator/meeting/meeting.ts
@@ -1,22 +1,20 @@
-import { getDate, getDMYDateString, getWeekNumber } from "../dep.ts";
-import { agendaMarkdown, meetingMarkdown } from "./markdown.ts";
+import { getDate, getDMYDateString, getWeekNumber } from '../dep.ts'
+import { agendaMarkdown, meetingMarkdown } from './markdown.ts'
 
 export async function createMeeting() {
-  console.log("Creating meeting....\n");
-  await Deno.mkdir("meetings").catch(() => {});
-  const d = await getDate();
-  const dateString = getDMYDateString(d, "/");
+  console.log('Creating meeting....\n')
+  await Deno.mkdir('meetings').catch(() => {})
+  const d = await getDate()
+  const dateString = getDMYDateString(d, '/')
   const getMeetingFileName = (prefix: string, date: Date) =>
-    `meetings/[${prefix} ${getWeekNumber(date)} ${
-      getDMYDateString(date, " ")
-    }].md`;
+    `meetings/${getWeekNumber(date)}_${prefix}_${getDMYDateString(date, '_')}.md`
 
   Deno.writeFile(
-    `${getMeetingFileName("S", d)}`,
-    new TextEncoder().encode(meetingMarkdown(dateString)),
-  );
+    `${getMeetingFileName('S', d)}`,
+    new TextEncoder().encode(meetingMarkdown(dateString))
+  )
   Deno.writeFile(
-    `${getMeetingFileName("A", d)}`,
-    new TextEncoder().encode(agendaMarkdown(dateString)),
-  );
+    `${getMeetingFileName('A', d)}`,
+    new TextEncoder().encode(agendaMarkdown(dateString))
+  )
 }
